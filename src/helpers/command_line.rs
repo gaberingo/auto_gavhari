bc61da66b1b27638f75e8407b0d91eebac677684
gaberingo <gaberingo12@gmail.com>
2025-03-03 00:10:58 +0700

    96. Print Agent Activity to Command Line

--- src/helpers/command_line.rs
+++ src/helpers/command_line.rs
@@ -2,9 +2,40 @@ use std::io::{stdin, stdout};
 
 // crossterm digunakan untuk pemberian warna pada terminal
 use crossterm::{
-    style::{Color, ResetColor, SetForegroundColor},
+    style::{Color, Colors, ResetColor, SetForegroundColor},
     ExecutableCommand,
 };
+
+#[derive(PartialEq, Debug)]
+pub enum PrintCommand {
+    AICall,
+    UnitTest,
+    Issue,
+}
+
+impl PrintCommand {
+    pub fn print_agent_message(&self, agent_pos: &str, agent_statement: &str) {
+        let mut stdout: std::io::Stdout = stdout();
+
+        // Decide on the print Color
+        let statement_color = match self {
+            Self::AICall => Color::Cyan,
+            Self::UnitTest => Color::Magenta,
+            Self::Issue => Color::Red,
+        };
+
+        // Print the agent statement in a specific Color
+        stdout.execute(SetForegroundColor(Color::Green)).unwrap();
+        print!("Agent: {}: ", agent_pos);
+
+        // Make selected Color
+        stdout.execute(SetForegroundColor(statement_color)).unwrap();
+        println!("{}", agent_statement);
+
+        // Reset color
+        stdout.execute(ResetColor).unwrap();
+    }
+}
 
 // Get user request
 pub fn get_user_response(question: &str) -> String {
@@ -26,4 +57,14 @@ pub fn get_user_response(question: &str) -> String {
 
     // Trim whitespace and return
     return user_response.trim().to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tests_print_agent_msg() {
+        PrintCommand::AICall.print_agent_message("Managing Agent", "testing some output");
+    }
 }
--- src/helpers/generals.rs
+++ src/helpers/generals.rs
@@ -1,10 +1,31 @@
 use crate::models::general::llm::Message;
 
-pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) {
+pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
     let ai_function_str = ai_func(func_input);
+
+    let msg = format!(
+        "Function: {}
+INSTRUCTION: You are a function printer. You ONLY print the result of the functions. 
+Nothing else. No commentary. Here is the input to the function {}.
+OUTPUT: Print out what the function will return",
+        ai_function_str, func_input
+    );
+
+    Message {
+        role: "System".to_string(),
+        content: msg,
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::ai_functions::aifunc_managing::convert_user_input_to_goal;
+
+    #[test]
+    fn tests_extending_ai_function() {
+        let extended_msg = extend_ai_function(convert_user_input_to_goal, "todo app");
+        dbg!(&extended_msg);
+        assert_eq!(extended_msg.role, "System".to_string());
+    }
 }
